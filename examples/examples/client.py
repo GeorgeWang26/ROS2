756a5beea78365578dde72ecadef4bbb781f191c
George Wang <wzheng2013@gmail.com>
2022-06-07 11:31:50 -0400

    custom interface

--- examples/examples/client.py
+++ examples/examples/client.py
@@ -1,6 +1,7 @@
 import sys
 
-from example_interfaces.srv import AddTwoInts
+# from example_interfaces.srv import AddTwoInts
+from interfaces.srv import AddThreeInts
 import rclpy
 from rclpy.node import Node
 
@@ -9,14 +10,17 @@ class MinimalClientAsync(Node):
 
     def __init__(self):
         super().__init__('minimal_client_async')
-        self.cli = self.create_client(AddTwoInts, 'add_two_ints')
+        # self.cli = self.create_client(AddTwoInts, 'add_two_ints')
+        self.cli = self.create_client(AddThreeInts, "add_three_ints")
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
-        self.req = AddTwoInts.Request()
+        # self.req = AddTwoInts.Request()
+        self.req = AddThreeInts.Request()
 
     def send_request(self):
         self.req.a = int(sys.argv[1])
         self.req.b = int(sys.argv[2])
+        self.req.c = int(sys.argv[3])    # new in three_ints srv
         self.future = self.cli.call_async(self.req)
 
 
@@ -40,9 +44,12 @@ def main(args=None):
                 minimal_client.get_logger().info(
                     'Service call failed %r' % (e,))
             else:
+                # minimal_client.get_logger().info(
+                #     'Result of add_two_ints: for %d + %d = %d' %
+                #     (minimal_client.req.a, minimal_client.req.b, response.sum))
                 minimal_client.get_logger().info(
-                    'Result of add_two_ints: for %d + %d = %d' %
-                    (minimal_client.req.a, minimal_client.req.b, response.sum))
+                    'Result of add_three_ints: for %d + %d + %d = %d' %
+                    (minimal_client.req.a, minimal_client.req.b, minimal_client.req.c, response.sum))
             break
 
     minimal_client.destroy_node()
